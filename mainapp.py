80a6ac8a6e15fef56febeb732b4c0b8e341c3c1e
605553972 <605553972@qq.com>
2020-08-18 23:44:26 +0800

    try for

--- mainapp.py
+++ mainapp.py
@@ -155,4 +155,4 @@ def Ada_predict():
 app.config.from_pyfile("config.cfg")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
